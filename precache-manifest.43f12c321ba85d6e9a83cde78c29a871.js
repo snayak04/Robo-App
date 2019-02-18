68b15aefa6ab940a1b03ee313db0fe00737aa101
Swarup Nayak <snayak04.96@gmail.com>
2019-02-18 16:16:34 -0500

    Updates

---
+++ precache-manifest.43f12c321ba85d6e9a83cde78c29a871.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ccd0cb4139034b2b449",
+    "url": "/Robo-App/static/css/main.d4aa66f1.chunk.css"
+  },
+  {
+    "revision": "5ccd0cb4139034b2b449",
+    "url": "/Robo-App/static/js/main.5ccd0cb4.chunk.js"
+  },
+  {
+    "revision": "933adea8a6562d40bcb9",
+    "url": "/Robo-App/static/css/1.829c47e0.chunk.css"
+  },
+  {
+    "revision": "933adea8a6562d40bcb9",
+    "url": "/Robo-App/static/js/1.933adea8.chunk.js"
+  },
+  {
+    "revision": "890e7f2273d55a73f832",
+    "url": "/Robo-App/static/js/runtime~main.890e7f22.js"
+  },
+  {
+    "revision": "9c3e25ca34c7666925bdb7e9cda32d08",
+    "url": "/Robo-App/static/media/geosteam.9c3e25ca.ttf"
+  },
+  {
+    "revision": "f94d00ecde9883d7549a565c711bd35e",
+    "url": "/Robo-App/index.html"
+  }
+];
