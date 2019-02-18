68e7760fed665a590d1feddd127886b28821269b
Swarup Nayak <snayak04.96@gmail.com>
2019-02-18 14:49:10 -0500

    Complete Robo App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
-
+import './App.css';
+import Loading from './Loading';
+import Scroll from './Scroll';
 
 class App extends Component{ 
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ""
         }
     };
+
 /*Any custom function in JSX has to follow the arrow funcitons *
 @updated this.searchField value*/
     onSearch = (event) => {
         //setting state:
         this.setState({ searchField: event.target.value});
     };
+    
+    /*
+    The cycle is:
+    1. Constructor
+    2. static getDerivedStateFromProps()
+    3. render()
+    4. componentDidMount()
+    if componenetDidMount() then render() aagain.
+    */
+    componentDidMount(){
+        //Using API to getch response.
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users})); 
+        //if used without {, they will return by default
+    }
 
     render(){
         var filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
         });
+        if(!this.state.robots.length){
+            return <Loading />
+        }else{
         return(
             <div className="tc">
-                <h1>Robo App</h1>
+                <h1 className="f1">Robo App</h1>
                 <SearchBox searchChange={this.onSearch} />
-                { <CardList robots={filteredRobots}/> /*Abstracting Cards into CardList with CardList being the parent, and Cards its child. */ }
+                <Scroll>
+                    { <CardList robots={filteredRobots}/> /*Abstracting Cards into CardList with CardList being the parent, and Cards its child. */ }
+                </Scroll>
             </div>
         );
+        }
     }
 }
 
